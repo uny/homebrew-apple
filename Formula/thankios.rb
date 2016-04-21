66a6650e952ca00d6ece33c8e66994c3e75160bb
Yuki Nagai <ynagai@r.recruit.co.jp>
2016-04-21 18:19:53 +0900

    thankios v1.0.1

--- Formula/thankios.rb
+++ Formula/thankios.rb
@@ -1,9 +1,9 @@
 class Thankios < Formula
   desc "Acknowlegements Settings.bundle generator for CocoaPods and Carthage."
   homepage "https://github.com/uny/Thankios"
-  url "https://github.com/uny/Thankios/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "e9aed4c539d6e93cbe16f58017bde4102c43b20dd6d5b9599e4bb15a237d6d40"
+  url "https://github.com/uny/Thankios/archive/v1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "b136adaae8d17ad79e0bba9232e3c04c2025f969f143e99462515935b378e3cd"
 
   depends_on :xcode => ["7.2", :build]
 
